11f0436309e8563cba1908e8efa319dc5ac475b9
kingp06 <moneyismoney04@gmail.com>
2026-07-12 04:36:57 -0400

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Alaska State Roleplay",
+        name: "🌴Alaska State Roleplay",
         // Activity type number (0 = Playing).
         type: 3,
       },
